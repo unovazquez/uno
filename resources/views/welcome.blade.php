c71e17889cc066006a93cc236c2c8b65204d78a8
Usuario <user1@iMac-de-Usuario.local>
2016-06-06 16:21:03 -0500

    Push inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Bienvenidos a UNO</div>
+
+                <div class="panel-body">
+Esta es una vista ve todas las vistas dentro de la carpeta views y velas abriendo a ver que modificas y que agregas.                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
