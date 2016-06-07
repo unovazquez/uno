9b4044c95187744fab0cb7af89528cff296a6e96
Usuario <user1@iMac-de-Usuario.local>
2016-06-07 00:03:43 -0500

    perro en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,6 +10,8 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
+                        <h1>PERROO</h1>
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
