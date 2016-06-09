fedb3769c5f0c8f672b37ec2c9eaa1b63bd4f044
Luis Gerardo <lsgrrd@gmail.com>
2016-06-09 14:48:26 -0500

    analytics

--- resources/views/welcomeuno.blade.php
+++ resources/views/welcomeuno.blade.php
@@ -399,8 +399,10 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-69039707-1', 'auto');
+  ga('create', 'UA-78985893-1', 'auto');
   ga('send', 'pageview');
+
+</script>
 
 </script>
 
